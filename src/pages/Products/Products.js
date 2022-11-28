e0357c94193806a7acb1b04f43bd5d76c77916e2
MrEmin <emuhammet.ozdemir@gmail.com>
2022-11-28 10:44:56 +0300

    fixed

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {FlatList} from 'react-native';
+import {View, FlatList} from 'react-native';
 import {API_URL} from '@env';
 
 import ProductCard from '../../components/ProductCard';
 import useFetch from '../../hooks/useFetch';
 import Loading from '../../components/Loading';
 import Error from '../../components/Error';
+import SearchBar from '../../components/SearchBar';
 
 const Products = ({navigation}) => {
   const {loading, data, error} = useFetch(API_URL);
@@ -26,7 +27,12 @@ const Products = ({navigation}) => {
     return <Error />;
   }
 
-  return <FlatList data={data} renderItem={renderProduct} />;
+  return (
+    <View>
+      <SearchBar />
+      <FlatList data={data} renderItem={renderProduct} />
+    </View>
+  );
 };
 
 export default Products;
